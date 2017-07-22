8c126d534f04fc3e8ffba7527e0824cffa15629b
cisarbriones <cisarbriones@hotmail.com>
2017-07-22 07:57:17 -0400

    Anadi el parametro url a la app short

--- short/admin.py
+++ short/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Acortar
 # Register your models here.
+
+admin.site.register(Acortar)
--- short/models.py
+++ short/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
 # Create your models here.
+class Acortar(models.Model):
+	url = models.CharField(max_length=220,)
+
+	def __str__(self):
+		return (self.url)
