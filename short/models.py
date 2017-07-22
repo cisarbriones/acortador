8ebaa1ee9473fcf4244a4fa7858e20b45af82614
cisarbriones <cisarbriones@hotmail.com>
2017-07-22 08:06:29 -0400

    Anadi al funcion de codigo unico en la app short

--- short/models.py
+++ short/models.py
@@ -2,6 +2,8 @@ from django.db import models
 # Create your models here.
 class Acortar(models.Model):
 	url = models.CharField(max_length=220,)
+	code = models.CharField(max_length=15, unique=True)
+
 
 	def __str__(self):
 		return (self.url)
